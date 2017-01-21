550f06a9ef7175d9c896304e2afd6611cd652b16
EngineerCoding <engineercoding@hotmail.nl>
2017-01-21 13:44:26 +0100

    Revert "Make the Facebook API reusable and implement basis of index feed"
    This reverts commit 3eaf63e91db74f71c1590dba17a20f5bb95ff175.

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
 
 Route::get('/', function () {
-    $feed = (new \SVExon\Http\FacebookFeed())->get_feed();
-    if (isset($feed["error"])) {
-        $fake_feed = array(
-            "feed" => array(),
-            "new_uuid" => "fetch",
-            "old_uuid" => "fetch"
-        );
-        return View::make('index')->with("feed", $fake_feed);
-    }
-    return View::make('index', compact("feed"));
+    return View::make('welcome');
 });
