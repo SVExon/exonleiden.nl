53809101c2191cf6ad301b008625192c78585efd
EngineerCoding <engineercoding@hotmail.nl>
2017-01-22 02:49:25 +0100

    Start on client development

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,7 @@
 const elixir = require('laravel-elixir');
 
-require('laravel-elixir-vue-2');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(mix = > {
-    mix.sass('app.scss')
-    .webpack('app.js');
-})
-;
+elixir(function(mix) {
+    mix.scripts(['feed_handler.js'], 'public/js/feed_handler.min.js')
+        .version('public/js/feed_handler.min.js');
+});
