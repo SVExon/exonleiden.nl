77e55e0c7769cd68217bea1308ddcd3f78fdcc0c
EngineerCoding <engineercoding@hotmail.nl>
2017-01-25 23:56:49 +0100

    Implement basic index page

--- gulpfile.js
+++ gulpfile.js
@@ -4,4 +4,6 @@ const elixir = require('laravel-elixir');
 elixir(function(mix) {
     mix.scripts(['feed_handler.js', 'moment-with-locales.js'], 'public/js/index.min.js')
         .version('public/js/index.min.js');
+    mix.sass('index.sass', 'public/css/index.min.css')
+        .version('public/css/index.min.css');
 });
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <title>SV Exon</title>
+        <link  href="{{ elixir('css/index.min.css') }}" rel="stylesheet" type="text/css"/>
         <script src="{{ URL::asset('js/jquery-3.1.1.min.js') }}"></script>
         <script src="{{ elixir('js/index.min.js') }}"></script>
         <script>
@@ -10,7 +11,36 @@
         </script>
     </head>
     <body>
-        <div id="postContainer">
+        <nav class="fixed">
+            <ul>
+                <li class="active"><a href>Home</a></li>
+                <li class="symbol">&#164;</li>
+                <li><a href>Het Bestuur</a></li>
+                <li class="symbol">&#164;</li>
+                <li><a href>Statuten</a></li>
+                <li class="symbol">&#164;</li>
+                <li><a href>Contact</a></li>
+            </ul>
+        </nav>
+        <div id="fixedWelcome" class="fixed">
+            <div id="logo" class="adjust">
+                <img src="{{ URL::asset('image/exon_logo.png') }}"/>
+            </div>
+        </div>
+        <input type="checkbox" id="close"/>
+        <div id="studieStore">
+            <a href="https://goo.gl/HHziJo" target="_blank">
+                <img src="{{ URL::asset('image/study_store_banner.jpg') }}" width="300" height="250">
+            </a>
+            <label for="close">
+                <img src="{{ URL::asset('image/close.png') }}" width="25" height="25"/>
+            </label>
+        </div>
+        <div id="scrollAbove">
+            {{--<div id="postContainer"></div>--}}
+            {{--<footer>--}}
+                {{--<p>Background provided by <a href="https://subtlepatterns.com/page/12/">Devin Holmes</a></p>--}}
+            {{--</footer>--}}
         </div>
     </body>
 </html>
